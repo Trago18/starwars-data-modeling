efb407d16b8957236db6641d19c183956b60b2db
Trago18 <franksolorc@hotmail.com>
2021-03-25 20:52:00 +0000

    Add files

--- src/models.py
+++ src/models.py
@@ -8,23 +8,50 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+class Favorite(Base):
+    __tablename__ = 'favorite'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    characters = Column(String(250))
+    planets = Column(String(250))
+
+class Character(Base):
+    __tablename__ = 'character'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), ForeignKey('favorite.characters'))
+    birth_day = Column(String(250))
+    gender = Column(String(250))
+    height = Column(Integer)
+    skin_color = Column(String(250))
+    hair_color = Column(String(250))
+    eye_color = Column(String(250))
+
+
+class Planet(Base):
+    __tablename__ = 'Planet'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), ForeignKey('favorite.planets'))
+    climate = Column(String(250))
+    population = Column(Integer)
+    terrain = Column(String(250))
+    rotation_period = Column(Integer)
+    orbital_period = Column(Integer)
+    diameter = Column(Integer)
 
     def to_dict(self):
         return {}
